a0db5c48b62078244f04ba0f737def76afe997cf
ZhengHao <forcelesszheng@foxmail.com>
2024-05-27 20:09:39 +0800

    add peft support eval

--- opencompass/eval_config.py
+++ opencompass/eval_config.py
@@ -5,7 +5,7 @@ with read_base():
     from .configs.datasets.triviaqa.triviaqa_wiki_gen_d18bf4 import triviaqa_datasets
     from .configs.datasets.gsm8k.gsm8k_gen_3309bd import gsm8k_datasets
     from .configs.datasets.humaneval.humaneval_gen_a82cae import humaneval_datasets
-    from .configs.datasets.agieval.agieval_mixed_2f14ad import agieval_datasets
+    from .configs.datasets.agieval.agieval_gen import agieval_datasets
     from .configs.datasets.SuperGLUE_BoolQ.SuperGLUE_BoolQ_ppl_314797 import (
         BoolQ_datasets,
     )
@@ -17,22 +17,21 @@ with read_base():
     # from .configs.datasets.nq.nq_open_gen_e93f8a import nq_datasets
 from opencompass.models import HuggingFaceCausalLM
 
-path = "/ciphome/zhuqiming/workspace/mindsft/sftout/llama-2-7b_sft_2024_5_17_6_2/checkpoint-5730"
+path = "~/Merge-MoE/smol_llama-4x220M-MoE"
 models = [
     dict(
         type=HuggingFaceCausalLM,
-        abbr="zhuque2-13B-dpo",
+        abbr="eve_smol_base",
         path=path,
+        peft_path=None,
         tokenizer_path=path,
         tokenizer_kwargs=dict(
-            padding_side="left",
-            truncation_side="left",
             use_fast=False,
         ),
         max_out_len=100,
         max_seq_len=4096,
-        batch_size=1,
-        model_kwargs=dict(device_map="npu:0", torch_dtype="torch.bfloat16"),
+        batch_size=16,
+        model_kwargs=dict(torch_dtype="torch.bfloat16"),
         batch_padding=True,  # if false, inference with for-loop without batch padding
         run_cfg=dict(num_gpus=1, num_procs=1),
     )
@@ -41,4 +40,4 @@ models = [
 datasets = sum(
     [v for k, v in locals().items() if k.endswith("_datasets") or k == "datasets"], []
 )
-work_dir = "./zhuque2_sft/"
+work_dir = "./eve_models/"
